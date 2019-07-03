d83dfc41d55a16836d8c53d413b0a91519d5df8c
Dan Immortal <36762815+ImmortalDan@users.noreply.github.com>
2019-07-03 17:32:51 +0300

    Add files via upload

---
+++ Lab 2.4.py
@@ -0,0 +1,27 @@
+# Python code and functions
+# This program will demonstrate various ways to
+# use functions in Python
+
+# The main function
+def main():
+    welcome_message() # causes welcome_message to run
+    goodbye_message()
+# This function is to welcome people to my program
+def welcome_message():
+    print('Welcome to my program using functions')
+    print('My name is Dan Munene')
+
+# This is the goodbye function
+
+def goodbye_message():
+    print('That was my program goodby for now')
+
+# This is the main function that starts the program
+# in motion
+
+main()
+
+
+
+
+
