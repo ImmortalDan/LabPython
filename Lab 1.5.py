904617981f074093ad734bfcdabeb1c583ae7fc8
Dan Immortal <36762815+ImmortalDan@users.noreply.github.com>
2019-07-03 17:13:33 +0300

    Add files via upload

---
+++ Lab 1.5.py
@@ -0,0 +1,11 @@
+# Lab 1.5 Programming Challenge 1 -Team Average
+
+firstYearWins = int(input('Enter number of wins in the first year.'))
+secondYearWins = int(input('Enter number of wins in the second year.'))
+thirdYearWins = int(input('Enter number of wins in the third year.'))
+fourthYearWins = int(input('Enter number of wins in the fourth year.'))
+fifthYearWins = int(input('Enter number of wins in the fifth year.'))
+
+averageWins = (firstYearWins + secondYearWins + thirdYearWins +fourthYearWins + fifthYearWins) / 5
+
+print("The average number of wins in the last five years is", averageWins)
