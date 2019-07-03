4e7edff3c79b117c8898640eb3dabd07cb7acb7e
Dan Immortal <36762815+ImmortalDan@users.noreply.github.com>
2019-07-03 17:21:12 +0300

    Add files via upload

---
+++ lab 1.6.py
@@ -0,0 +1,15 @@
+# lab 1.6 Programming Challenge 2 -Pedometer calculator
+
+dayoftheWeek = input('Enter day of the week')
+numberofSteps = int(input("Enter number of steps walked. "))
+
+numberofMiles = numberofSteps / 2000
+
+caloriesLost = numberofMiles * 65
+
+print("The day of the week is ", dayoftheWeek)
+print("The distance walked in miles is", numberofMiles)
+print("The calories lost are", caloriesLost)
+
+
+
